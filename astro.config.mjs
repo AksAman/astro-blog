173fe79deff9e83aecd075315a82c94f7ceccffd
AksAman@personal_pc <krsingh.aman@gmail.com>
2023-10-16 03:37:34 +0530

    fix site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
-const baseSite = PUBLIC_VERCEL_URL ? `https://${PUBLIC_VERCEL_URL}` : "http://localhost:4321"
+const baseSite = import.meta.env.PUBLIC_VERCEL_URL ? `https://${import.meta.env.PUBLIC_VERCEL_URL}` : "http://localhost:4321"
 
 // https://astro.build/config
 export default defineConfig({
